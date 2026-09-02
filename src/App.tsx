6830768894c5bdde94d0555ad08d4ed75ef078a1
jisusin <sjsz0811@hanyang.ac.kr>
2026-09-02 16:14:01 +0900

    feat: 공통 헤더 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
 import Landing from './pages/Landing'
 import Input from './pages/Input'
 import Result from './pages/Result'
@@ -6,6 +7,7 @@ import Result from './pages/Result'
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/input" element={<Input />} />
